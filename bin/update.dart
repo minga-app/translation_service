0d112d857acbfba23062da08072caf0aaf5dd998
Lukas Himsel <lukas@himsel.me>
2020-11-05 13:03:57 +0100

    parse

--- bin/update.dart
+++ bin/update.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:gsheets/gsheets.dart';
 import 'package:translation_service/arb_serialization.dart';
 import 'package:translation_service/credentials.dart';
+import 'package:translation_service/extract_messages.dart';
 
 final PATH = './lib/l10n';
 
@@ -19,18 +20,28 @@ main() async {
   for (var sheet in doc.sheets) {
     var context = sheet.title;
     var allColumns = await sheet.values.allColumns();
-    var keys = allColumns[0].sublist(1);
+
+    var sheetsKeys = allColumns[0].sublist(1);
     for (var col in allColumns.sublist(1)) {
       var locale = col.first;
       var sub = col.sublist(1);
       var values = List.generate(
-        keys.length,
+        sheetsKeys.length,
         (index) => sub.length > index ? sub[index] : '',
       );
-      var data = Map.fromIterables(keys, values);
+      var data = Map.fromIterables(sheetsKeys, values);
       var serializer = ArbSerializer.parse(locale, data, context);
       await File('$PATH/intl_$locale.arb')
           .writeAsString(serializer.serialize());
+
+      var rowCount = sheet.rowCount;
+      var messages = extractMessages('./lib/main.dart');
+      for (var key in messages.keys) {
+        if (sheetsKeys.contains(key)) {
+          await sheet.insertRow(rowCount + 1);
+          // insert new key here
+        }
+      }
     }
   }
   return;
